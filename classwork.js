969562b752ea5790d240aa88c9dc365f857d8388
Gerrard <privatesystem07@gmail.com>
2025-03-21 02:56:04 -0700

    classwork

--- classwork.js
+++ classwork.js
@@ -25,14 +25,31 @@
 // numberCheck(8);
 // const numberChecker = () => {};
 
-function numberCheck(x) {
-  if (x === 0) {
-    console.log("number is 0");
-  } else if (x % 2 === 0) {
-    console.log(` ${x} is Even`);
-  } else if (x % 2 !== 0) {
-    console.log(`${x} is Odd`);
+// function numberCheck(x) {
+//   if (x === 0) {
+//     console.log("number is 0");
+//   } else if (x % 2 === 0) {
+//     console.log(` ${x} is Even`);
+//   } else if (x % 2 !== 0) {
+//     console.log(`${x} is Odd`);
+//   }
+// }
+
+// numberCheck(-77);
+
+// 1.	Write a program that checks if a given year is a leap year.
+// 2.	Write a program that determines whether a person is eligible to vote based on their age.
+// 3.	Write a program that compares two numbers and prints the larger one.
+// 4.	Write a program that checks if a number is divisible by both 3 and 5.
+// 5.	Write a program that determines whether a given character is a vowel or a consonant.
+// 6.	Write a program that converts a temperature from Celsius to Fahrenheit.
+
+function checkLeap(year) {
+  if (year % 4 === 0) {
+    console.log("year is a leap year");
+  } else if (year % 4 !== 0) {
+    console.log("year is not a leap year");
   }
 }
 
-numberCheck(-77);
+checkLeap(1995);
