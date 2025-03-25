49ab5bed65b4092d67dbe85e9f6645611c24d794
Gerrard <privatesystem07@gmail.com>
2025-03-25 00:17:03 -0700

    Destructuring

--- script.js
+++ script.js
@@ -148,22 +148,32 @@ console.log(BMIMark > BMIJohn);
 
 // DOM document object model
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".check").addEventListener("click", () => {
-  const userGuess = Number(document.querySelector(".guess").value);
+// const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector(".check").addEventListener("click", () => {
+//   const userGuess = Number(document.querySelector(".guess").value);
 
-  if (!userGuess) {
-    document.querySelector(".message").innerHTML =
-      "Input a valid number in the box";
-  } else if (userGuess === secretNumber) {
-    document.querySelector(".message").innerHTML = "You don get am! Hurray 🥇";
-    document.querySelector(".message").style.backgroundColor = "green";
-  } else if (userGuess > 20) {
-    document.querySelector(".message").innerHTML =
-      " eye dey pain you? we say between 1 and 20";
-  } else if (userGuess > secretNumber) {
-    document.querySelector(".message").innerHTML = "guess too high";
-  } else if (userGuess < secretNumber) {
-    document.querySelector(".message").innerHTML = "guess too low";
-  }
-});
+//   if (!userGuess) {
+//     document.querySelector(".message").innerHTML =
+//       "Input a valid number in the box";
+//   } else if (userGuess === secretNumber) {
+//     document.querySelector(".message").innerHTML = "You don get am! Hurray 🥇";
+//     document.querySelector(".message").style.backgroundColor = "green";
+//   } else if (userGuess > 20) {
+//     document.querySelector(".message").innerHTML =
+//       " eye dey pain you? we say between 1 and 20";
+//   } else if (userGuess > secretNumber) {
+//     document.querySelector(".message").innerHTML = "guess too high";
+//   } else if (userGuess < secretNumber) {
+//     document.querySelector(".message").innerHTML = "guess too low";
+//   }
+// });
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Visa Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Focaccia", "Brushetta", "Garlic bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+};
+console.log(restaurant.location);
+const myBestfood = restaurant.mainMenu;
+console.log(myBestfood);
