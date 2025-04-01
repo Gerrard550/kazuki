9bfd6d816a05b0bcfe968f98e0ac5fe329f65f25
Gerrard <privatesystem07@gmail.com>
2025-04-01 08:31:22 -0700

    entries

--- classwork.js
+++ classwork.js
@@ -236,3 +236,113 @@ function checkInput(myState) {
 }
 
 console.log(checkInput("lagos"));
+
+const usaStates = [
+  "Alabama",
+
+  "Alaska",
+
+  "Arizona",
+
+  "Arkansas",
+
+  "California",
+
+  "Colorado",
+
+  "Connecticut",
+
+  "Delaware",
+
+  "Florida",
+
+  "Georgia",
+
+  "Hawaii",
+
+  "Idaho",
+
+  "Illinois",
+
+  "Indiana",
+
+  "Iowa",
+
+  "Kansas",
+
+  "Kentucky",
+
+  "Louisiana",
+
+  "Maine",
+
+  "Maryland",
+
+  "Massachusetts",
+
+  "Michigan",
+
+  "Minnesota",
+
+  "Mississippi",
+
+  "Missouri",
+
+  "Montana",
+
+  "Nebraska",
+
+  "Nevada",
+
+  "New Hampshire",
+
+  "New Jersey",
+
+  "New Mexico",
+
+  "New York",
+
+  "North Carolina",
+
+  "North Dakota",
+
+  "Ohio",
+
+  "Oklahoma",
+
+  "Oregon",
+
+  "Pennsylvania",
+
+  "Rhode Island",
+
+  "South Carolina",
+
+  "South Dakota",
+
+  "Tennessee",
+
+  "Texas",
+
+  "Utah",
+
+  "Vermont",
+
+  "Virginia",
+
+  "Washington",
+
+  "West Virginia",
+
+  "Wisconsin",
+
+  "Wyoming",
+];
+
+// for (const num of usaStates.entries()) {
+//   console.log(`Position ${num[0] + 1} is ${num[1]}`);
+// }
+
+for (const brief of usaStates.entries()) {
+  console.log(`${brief[0] + 1}th state is ${brief[1]}`);
+}
